118b90ebadd1f74d1b88766e2638674cb2ac3ff3
Kars1996 <fgghfdf741@gmail.com>
2023-04-18 20:48:40 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center" id="top"> 
+<img src="./bega.png">
+
+</br>
+
+<h1 align="center">Coming Soon</h1>
+:wink:
---
+++ optimiser.py
@@ -0,0 +1,10 @@
+print('Hello World')
+#Todo:
+#Get user's client
+#Edit some of the settings (ie render distance, etc etc)
+#Download the options.yml file from a git repo
+#replace that file in their client
+#Repeat if user requested
+#Reccomended settings system
+#Settings for streamers and recording
+# Diffrent presets (Fun times innit)
