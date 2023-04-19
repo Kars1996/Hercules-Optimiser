50b56e8073e9770a13b58f1ff6500a2cb4e2d39a
Kars1996 <fgghfdf741@gmail.com>
2023-04-19 11:50:29 +0100

    Update optimiser.py

--- optimiser.py
+++ optimiser.py
@@ -1,1 +1,26 @@
-print('Coming Soon')
+import os, getpass
+def blue(text):
+    os.system(""); faded = ""
+    for line in text.splitlines():
+        green = 0
+        for character in line:
+            green += 3
+            if green > 255:
+                green = 255
+            faded += (f"\033[38;2;0;{green};255m{character}\033[0m")
+        faded += "\n"
+    return faded
+
+banner = '''                  :::    ::: :::::::::: :::::::::   ::::::::  :::    ::: :::        :::::::::: :::::::: 
+                 :+:    :+: :+:        :+:    :+: :+:    :+: :+:    :+: :+:        :+:       :+:    :+: 
+                +:+    +:+ +:+        +:+    +:+ +:+        +:+    +:+ +:+        +:+       +:+         
+               +#++:++#++ +#++:++#   +#++:++#:  +#+        +#+    +:+ +#+        +#++:++#  +#++:++#++   
+              +#+    +#+ +#+        +#+    +#+ +#+        +#+    +#+ +#+        +#+              +#+    
+             #+#    #+# #+#        #+#    #+# #+#    #+# #+#    #+# #+#        #+#       #+#    #+#     
+            ###    ### ########## ###    ###  ########   ########  ########## ########## ########       '''
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print(blue(banner))
+print(blue('        [$] Under Construction'))
+getpass.getpass(prompt=blue('        [+] Press enter to exit the program'))
+quit()
